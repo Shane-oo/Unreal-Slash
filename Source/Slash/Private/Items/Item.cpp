7f5979e34182d3068a42d63dfd1bed846b81db34
Shane <shanemonck11@hotmail.com>
2024-05-04 11:37:17 +0800

    Initial Create

---
+++ Source/Slash/Private/Items/Item.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+#include "Slash/Slash.h"
+
+// #region Constructors
+AItem::AItem()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+// #endregion
+
+// #region Protected Methods
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+    Super::BeginPlay();
+
+    const FVector Location = GetActorLocation();
+    const FVector Forward = GetActorForwardVector();
+
+    DRAW_SPHERE(Location);
+    DRAW_VECTOR(Location, Location + Forward * 100.f);
+}
+
+// #endregion
+
+
+// #region Public Methods
+
+// Called every frame
+void AItem::Tick(const float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
