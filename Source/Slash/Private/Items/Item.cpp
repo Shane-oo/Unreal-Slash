b74a1785e7741c6f71cac819c6f24e294a2b82c1
Shane <shanemonck11@hotmail.com>
2024-05-04 11:43:58 +0800

    Refactoring

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,9 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Items/Item.h"
 
-#include "Slash/Slash.h"
+#include "Slash/DebugMacros.h"
 
 // #region Constructors
 AItem::AItem()
--- Source/Slash/Slash.h
+++ Source/Slash/Slash.h
@@ -3,11 +3,3 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
-#define DRAW_SPHERE(Location) if(const UWorld* World = GetWorld()) DrawDebugSphere(World, Location, 25.f, 12, FColor::Red, true);
-#define DRAW_LINE(StartLocation, EndLocation) if(const UWorld* World = GetWorld()) DrawDebugLine(World, StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
-#define DRAW_POINT(Location) if(const UWorld* World = GetWorld()) DrawDebugPoint(World, Location, 15.f, FColor::Red, true);
-#define DRAW_VECTOR(StartLocation, EndLocation)  {\
-        DRAW_LINE(StartLocation, EndLocation); \
-        DRAW_POINT(EndLocation); \
-    }
