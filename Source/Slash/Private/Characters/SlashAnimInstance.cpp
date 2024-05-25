4a47ee7c6e102970f618252bb0d10407e4a66adc
Shane <shanemonck11@hotmail.com>
2024-05-25 12:48:32 +0800

    Animation Instance

---
+++ Source/Slash/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashAnimInstance.h"
+#include "Characters//SlashCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// #region Public Methods
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+
+    SlashCharacter = Cast<ASlashCharacter>(TryGetPawnOwner());
+    if (SlashCharacter)
+    {
+        SlashCharacterMovement = SlashCharacter->GetCharacterMovement();
+    }
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(const float DeltaSeconds)
+{
+    Super::NativeUpdateAnimation(DeltaSeconds);
+
+    if (SlashCharacterMovement)
+    {
+        GroundSpeed = UKismetMathLibrary::VSizeXY(SlashCharacterMovement->Velocity);
+    }
+}
+
+// #endregion
